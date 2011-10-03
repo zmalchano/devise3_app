fb8bb42dbf3568d33e1674fab0584d99ec25ef3e
Zach Malchano <zach.malchano@gmail.com>
2011-10-03 13:31:38 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Devise3App::Application.config.secret_token = '646c48a80cc80424c625059c0fbb60f87328377b1e6fae66cc9fb3b5524ce647f363ff58ca92228bd0122f3e8e9d679a7432e22af401426536f94dde8512b83a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Devise3App::Application.config.session_store :cookie_store, :key => '_devise3_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Devise3App::Application.config.session_store :active_record_store
